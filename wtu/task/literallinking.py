a930e678112fbae2d7bb8c730e68467b1ea1e8a2
Kai Rother <krother@techfak.uni-bielefeld.de>
2018-04-19 19:30:40 +0200

    Better LiteralLinking

--- wtu/task/literallinking.py
+++ wtu/task/literallinking.py
@@ -1,7 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from collections import defaultdict
 import io, csv
-from operator import attrgetter
+from operator import itemgetter
+import Levenshtein
 
 from wtu.task import Task
 from wtu.table import Table
@@ -17,77 +18,147 @@ class LiteralLinking(Task):
         # instantiate backend
         backend_name, backend_args = backend
         self.backend = LiteralLinking.backends_available[backend_name](**backend_args)
+        self.numeric_diff_cutoff = 200
+        self.string_edit_distance_cutoff = 50
 
-    def match_literal(self, ln_anno, properties):
-        anno_type = ln_anno['type']
-        matching_properties = []
+    def match_numeric(self, literal_value, ln_number):
+        try:
+            literal_value = float(literal_value)
+        except ValueError:
+            return []
 
-        if anno_type == 'numeric':
-            for property_uri, literal_type, literal_value in properties:
-                try:
-                    literal_value = float(literal_value)
-                    if ln_anno['number'] == literal_value:
-                        pass
-                except ValueError:
-                    pass
+        if literal_value == ln_number:
+            return ['numeric_exact']
 
-        elif anno_type == 'date':
-            normalized_date = '{:d}-{:02d}-{:02d}'.format(
-                ln_anno['year'], ln_anno['month'], ln_anno['day_of_month']
-            )
+        if literal_value != 0 and ln_number != 0:
+            high, low = max(literal_value, ln_number), min(literal_value, ln_number)
+            diff_percent = abs((high-low)/low*100)
 
-            for property_uri, literal_type, literal_value in properties:
-                if normalized_date == literal_value:
-                    matching_properties.append(property_uri)
+            if diff_percent <= self.numeric_diff_cutoff:
+                return ['numeric_diff={:.2f}%'.format(diff_percent)]
 
-        elif anno_type == 'value and unit':
-            for property_uri, literal_type, literal_value in properties:
-                try:
-                    literal_value = float(literal_value)
-                    if ln_anno['value_normalized'] == literal_value:
-                        matching_properties.append(property_uri)
-                except ValueError:
-                    pass
+        return []
+
+    def match_value_and_unit(self, literal_value, value, value_normalized):
+        return [
+            *self.match_numeric(literal_value, value),
+            *['normalized_' + transf for transf in self.match_numeric(literal_value, value_normalized)]
+        ]
+
+    def match_date(self, literal_value, date_parts):
+        # normalized the LN annotation's date to these formats
+        # for comparison
+        format_strings = {
+            'date_normal': '{:04d}-{:02d}-{:02d}',
+            'date_nozero': '{:d}-{:d}-{:d}',
+        }
+
+        matching_formats = []
+        # iterate over all formats and try to match the literal_value
+        # keep a record of all matching formats
+        for format_name, format_string in format_strings.items():
+            date_formatted = format_string.format(*date_parts)
+            if date_formatted == literal_value:
+                matching_formats.append(format_name)
+
+        return matching_formats
+
+    def match_string(self, literal_value, cell_content):
+        if literal_value == cell_content:
+            return ['string_exact']
+
+        if literal_value.lower() == cell_content.lower():
+            return ['string_ignore_case']
+
+        edit_distance = Levenshtein.distance(
+            literal_value.lower(),
+            cell_content.lower()
+        )
+        max_str_len = max(len(literal_value), len(cell_content))
+        edit_distance_percent = edit_distance*100/max_str_len
+
+        if edit_distance_percent <= self.string_edit_distance_cutoff:
+            return ['string_edit_distance={:.2f}%'.format(edit_distance_percent)]
+
+    def match_properties(self, cell, properties):
+        # find all LN annotations of the current cell
+        ln_annos = cell.find_annotations(anno_source='LiteralNormalization')
+
+        matching_properties = defaultdict(lambda : { 'index_value': None, 'transforms': [] })
+        # iterate over all properties
+        for property_uri, literal_type, literal_value in properties:
+            # iterate over all LN annotations the cell might have
+            # and try to match the 'normalized' versions of the cell's
+            # content to a propertie's literal_value
+            for ln_anno in ln_annos:
+                ln_type = ln_anno['type']
+
+                if ln_type == 'numeric':
+                    matching_numeric = self.match_numeric(literal_value, ln_anno['number'])
+                    if matching_numeric:
+                        matching_properties[property_uri]['transforms'].extend(matching_numeric)
+                        matching_properties[property_uri]['index_value'] = literal_value
+                elif ln_type == 'value and unit':
+                    matching_value_and_unit = self.match_value_and_unit(literal_value, ln_anno['value'], ln_anno['value_normalized'])
+                    if matching_value_and_unit:
+                        matching_properties[property_uri]['transforms'].extend(matching_value_and_unit)
+                        matching_properties[property_uri]['index_value'] = literal_value
+                elif ln_type == 'date':
+                    matching_date_formats = self.match_date(literal_value, [
+                        ln_anno[part] for part in [
+                            'year', 'month', 'day_of_month'
+                        ]]
+                    )
+                    if matching_date_formats:
+                        matching_properties[property_uri]['transforms'].extend(matching_date_formats)
+                        matching_properties[property_uri]['index_value'] = literal_value
+
+            # just compare the cell's content as it is
+            matching_string = self.match_string(literal_value, cell.content)
+            if matching_string:
+                matching_properties[property_uri]['transforms'].extend(matching_string)
+                matching_properties[property_uri]['index_value'] = literal_value
 
         return matching_properties
 
     def run(self, table):
         # iterate over all rows
         for row in table.rows():
-            el_cells = []  # holds cells with EntityLinking annotations
-            ln_cells = []  # holds cells with LiteralNormalization annotations
-
-            # iterate ovell cell in row
+            # find all 'entity' cell in the current row
+            el_cells = []
             for cell in row:
-                # if the cell has EntityLinking annotations add it to el_cells
                 el_annos = cell.find_annotations(anno_source='EntityLinking')
                 if el_annos:
                     el_cells.append((cell, el_annos))
-                    continue # skip looking for LiteralNormalization annotations
 
-                # if the cell has LiteralNormalization add it to ln_cells
-                ln_annos = cell.find_annotations(anno_source='LiteralNormalization')
-                if ln_annos:
-                    ln_cells.append((cell, ln_annos))
+            # iterate over all 'entity' cells
+            for el_cell, el_annos in el_cells:
+                # iterate over all EL annotations of this cell
+                for el_anno_idx, el_anno in enumerate(el_annos):
+                    # query the backend for the set of this entitie's properties,
+                    # skip this entity if there are none
+                    properties = self.backend.query(el_anno['uri'])
+                    if not properties:
+                        continue
 
-            # proceed if the row has both cell with EL annotations and LN annotations
-            if el_cells and ln_cells:
-                for el_cell, el_annos in el_cells:
-                    for el_anno_idx, el_anno in enumerate(el_annos):
-                        properties = self.backend.query(el_anno['uri'])
-                        for ln_cell, ln_annos in ln_cells:
-                            for ln_anno_idx, ln_anno in enumerate(ln_annos):
-                                for matching_property in self.match_literal(ln_anno, properties):
-                                    ln_cell.annotations.append({
-                                        'source': 'LiteralLinking',
-                                        'type': 'property',
-                                        'uri': matching_property,
-                                        'references': {
-                                            'EL': '{:d}:{:d}/{:d}'.format(*el_cell.idx, el_anno_idx),
-                                            'LN': '{:d}:{:d}/{:d}'.format(*ln_cell.idx, ln_anno_idx),
-                                        },
-                                    })
+                    # iterate over all other cells in this row
+                    # i.e. not the one containing the entity
+                    for other_cell in row:
+                        if other_cell.idx == el_cell.idx:
+                            continue
 
+                        # try to match the cell's content with one of
+                        # the entity's properties
+                        matching_properties = self.match_properties(other_cell, properties)
+                        for matching_property, property_info in matching_properties.items():
+                            other_cell.annotations.append({
+                                'source': 'LiteralLinking',
+                                'type': 'property',
+                                'uri': matching_property,
+                                'references_EL': '{:d}:{:d}/{:d}'.format(*el_cell.idx, el_anno_idx),
+                                'transforms': property_info['transforms'],
+                                'index_value': property_info['index_value'],
+                            })
         return True
 
 class LiteralLinkingBackend(metaclass=ABCMeta):
