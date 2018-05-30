98b5b1b1c783a19ec9a34c83ec29516971388090
Kai Rother <krother@techfak.de>
2018-05-30 17:06:36 +0200

    Stats and missed entities in evaluate_preprocessing.py
    evaluate_preprocessing.py now output a list of missed entities as well
    as some statistics (e.g. recall@k).

--- evaluate_preprocessing.py
+++ evaluate_preprocessing.py
@@ -2,18 +2,24 @@
 
 import io, sys, json
 from json.decoder import JSONDecodeError
+from collections import Counter
 
 from wtu.table import Table
 
-# collect stats
-num_tables = 0
+gold_source = 'gold-v2'
+preprocessing_source = 'preprocessing'
+
 total_entities = 0
-total_correct = 0
-total_only_one = 0
-total_highest_freq = 0
+correct_at_k = Counter()
+total_missed = 0
+missed_no_anno = 0
+
+missed_file = 'eva_prep.missed'
+stats_file = 'eva_prep.stats'
 
 # read from stdin, ignore encoding errors
-with io.open(sys.stdin.fileno(), 'r', encoding='utf-8', errors='ignore') as stdin:
+with io.open(sys.stdin.fileno(), 'r', encoding='utf-8', errors='ignore') as stdin, \
+io.open(missed_file, 'w') as missed_fh, io.open(stats_file, 'w') as stats_fh:
     # iterate over input. Each line represents one table
     for json_line in stdin:
         try:
@@ -22,65 +28,50 @@ with io.open(sys.stdin.fileno(), 'r', encoding='utf-8', errors='ignore') as stdi
             # create Table object to work with
             table = Table(table_data)
 
-            # collect stats
-            num_tables += 1
-            table_entities = 0
-            table_correct = 0
-            table_only_one = 0
-            table_highest_freq = 0
-
-            # iterate over all cells in the table
             for cell in table.cells():
-                # get EntityLinkning annotation from Gold Standard for this cell
                 gold_el = cell.find_annotations(
-                    anno_source='gold-v2',
-                    anno_task='EntityLinking'
+                    anno_source = gold_source,
+                    anno_task = 'EntityLinking'
                 )
                 if gold_el:
-                    table_entities += 1
-                    # extract resource uri from annotation
-                    gold_uri = gold_el[0]['resource_uri']
-                    # get EntityLinking annotations from our preprocessing
-                    preprocessing_el = cell.find_annotations(
-                        anno_source='preprocessing',
-                        anno_task='EntityLinking'
-                    )
-                    # extract resource uris and frequencies
-                    preprocessing_uris = {
-                        annotation['resource_uri']: annotation['frequency']
-                        for annotation in preprocessing_el
-                    }
+                    total_entities += 1
+                    gold_el = gold_el[0]
+                    gold_uri = gold_el['resource_uri']
 
-                    # check if our annotations also include the gold annotation
+                    preprocessing_uris = [
+                        el_anno['resource_uri'] for el_anno in
+                        cell.find_annotations(
+                            anno_source = preprocessing_source,
+                            anno_task = 'EntityLinking'
+                        )
+                    ]
+
                     if gold_uri in preprocessing_uris:
-                        table_correct += 1
-                        print('Correctly identified "{:s}" as {:s}'.format(
-                            cell.content, gold_uri
+                        for k in range(1, 101):
+                            preprocessing_uris_at_k = preprocessing_uris[0:k]
+                            if gold_uri in preprocessing_uris_at_k:
+                                correct_at_k[k] += 1
+                    else:
+                        total_missed += 1
+                        if len(preprocessing_uris) == 0:
+                            missed_no_anno += 1
+                        missed_fh.write('"{:s}" {:s} {:s}\n'.format(
+                            cell.content,
+                            gold_el['resource_uri'],
+                            str(preprocessing_uris)
                         ))
-                        # was it the only annotation?
-                        if len(preprocessing_uris) == 1:
-                            table_only_one += 1
-                            print('  * It was the only annotation')
-                        # had it the highest frequency?
-                        elif max(preprocessing_uris, key=preprocessing_uris.get) == gold_uri:
-                            table_highest_freq += 1
-                            print('  * It had the highest frequency')
-
-            # update stats
-            total_entities += table_entities
-            total_correct += table_correct
-            total_only_one += table_only_one
-            total_highest_freq += table_highest_freq
 
         # ignore json decoding errors
         except JSONDecodeError:
             pass
 
-    print(
-        '---',
-        'entitties   : {:5d} ({:.2f}/tbl)'.format(total_entities, total_entities/num_tables),
-        'correct     : {:5d} ({:.2f}/tbl)'.format(total_correct, total_correct/num_tables),
-        'only one    : {:5d} ({:.2f}/tbl)'.format(total_only_one, total_only_one/num_tables),
-        'highest freq: {:5d} ({:.2f}/tbl)'.format(total_highest_freq, total_highest_freq/num_tables),
-        sep='\n'
-    )
+    stats_fh.write('#total entities: {:d}\n#missed: {:d} ({:.2f}%)\n#no annotation: {:d} ({:.2f}%)\n\n'.format(
+        total_entities,
+        total_missed, total_missed * 100 / total_entities,
+        missed_no_anno, missed_no_anno * 100 / total_missed
+    ))
+
+    for k, count in correct_at_k.items():
+        stats_fh.write('recall@{:d}: {:.3f}\n'.format(
+            k, count / total_entities
+        ))
