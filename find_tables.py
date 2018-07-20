414a6e260af0c900d6a4a27177315d7b51328e12
Svenja <skoester@techfak.uni-bielefeld.de>
2018-07-20 11:22:25 +0200

    Prints the found table and at the end a summary of the number from the found tables and the rows that can be interesting

--- find_tables.py
+++ find_tables.py
@@ -7,18 +7,20 @@ import collections as cl
 # Searches for literal links without gold comparison
 def getLiteral_ng(cell):
     preprocessing = cell.find_annotations(anno_task='LiteralLinking')
-    return [annotation['property_uri'] for annotation in preprocessing]  
+
+    return list(set([annotation['property_uri'].replace('http://dbpedia.org/ontology/', '') for annotation in preprocessing]))
 
 # Searches for entity links without gold comparison
 def getEntity_ng(cell):
     preprocessing = cell.find_annotations(anno_source='preprocessing',anno_task='EntityLinking')
-    return [annotation['resource_uri'] for annotation in preprocessing]
+    return [annotation['resource_uri'].replace('http://dbpedia.org/resource/', '') for annotation in preprocessing]
 
 
 
-def analyzeTable(table): 
+def analyzeTable(table, g_col): 
     global num_found
     global found_tables
+    global found_tables_with_rows
 
     
     el_list = {}
@@ -55,11 +57,13 @@ def analyzeTable(table):
     
     res_rows = []
     keys = []
+    res_cols = []
 
     for i in res_table.keys():
         if len(res_table[i]) > 10:
             s = s + str(i) + " "
             keys = keys + [str(i)]
+            res_cols = list(set(res_cols + [i[4]] + [i[10]]))
             if not res_rows:
                 res_rows = res_table[i]
             if res_table[i]:
@@ -68,21 +72,41 @@ def analyzeTable(table):
     if res_rows and (len(res_rows)/len(table)) > 0.25 and len(keys) > 1:
         found_tables = found_tables + [num_tabel]
         num_found = num_found + 1
+        res_rows = sorted(res_rows)
         print(s)
         print("Keys Entity:        ", el_list.keys(), "\nKeys Literals:      ", ll_list.keys())
         print("Row Numbers:        ", res_rows)
-        print()
+        print("Col Numbers:        ", sorted(res_cols))
+        print("Col Label:          ", [g_col[i][0]['property_uri'].replace("http://dbpedia.org/ontology/", "") if str(g_col[i][0]['property_uri']) not in 'http://www.w3.org/2000/01/rdf-schema#label' else 'label' for i in range(0,len(g_col)) if g_col[i] and str(i) in res_cols], "\n")
+        #print("Col Label:          ", [g_col[i][0]['property_uri'] for i in range(0,len(g_col)) if g_col[i] and str(i) in res_cols], "\n")
+        printTableWithSelRows(table, res_rows, sorted(res_cols))
+        #printTable(table)
+        print("\n")
 
+        found_tables_with_rows = found_tables_with_rows + [{num_tabel:sorted(res_rows)}]
 
-num_tabel = 0
-num_found = 0
-found_tables = []
+def printTableWithSelRows(t, rows, cols):
+    print("Table:")
+    for i in range(0, len(t)):
+        if i in rows:
+            r = []
+            for j in range(0, len(t[0])):
+                if str(j) in cols:
+                    r = r + [t[i][j]]
+            print(i , ": ", r)
+    print('')
 
 def printTable(t):
     print("Table Num:" , num_tabel)
     for e in t:
         print(e)
     print('')
+
+num_tabel = 0
+num_found = 0
+found_tables = []
+
+found_tables_with_rows = []
 
 def main():
     global num_tabel
@@ -95,28 +119,29 @@ def main():
                 t = []
                 
                 col = [c.find_annotations(anno_task = 'PropertyLinking') for c in table.columns()]
+
                 for row in table.rows():
                     
-                    if row.find_annotations(anno_task='EntityLinking') is not []:
+                    if row.find_annotations(anno_source = 'preprocessing', anno_task='EntityLinking') is not []:
                         i = 0
                         r = []
                         for cell in row:
                             if col[i]:
-                                r = r + [[getEntity_ng(cell)]+[ getLiteral_ng(cell)]]
+                                r = r + [[getEntity_ng(cell)]+[getLiteral_ng(cell)]]
                                 i += 1
                         if r:
                             t = t + [r]
                 
                 if t :
-                    #printTable(t)
-                    analyzeTable(t)
+                    analyzeTable(t, col)
 
                 num_tabel += 1
 
             except JSONDecodeError:
                 pass
 
-    print("Found Tables: ", found_tables, ", Number: ", num_found)
+    print("Found Tables: ", found_tables, ", Number: ", num_found, "\n")
+    print(found_tables_with_rows)
 
 if __name__ == "__main__":
     main()
